15f48268156d4e241d032a7cd08f20757bbc414c
Diana <m96860351@gmail.com>
2025-09-01 13:18:16 -0600

    serv

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,24 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '0.0.0.0'),       
+  port: env.int('PORT', 1337),       
+  url: env('STRAPI_ADMIN_BACKEND_URL', 'https://cremeria-back.onrender.com'), // URL pública
+  admin: {
+    serveAdminPanel: true,
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+    url: env('ADMIN_PATH', '/admin'), 
+  },
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS'),      
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+
+  proxy: true,
+  cron: {
+    enabled: true,
   },
 });
+
